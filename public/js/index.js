19f72a47b66d1ddb1c44a82587fa5f8fbecf5c97
leedom <jld1221@163.com>
2017-09-12 00:01:13 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -153,6 +153,7 @@ const About = {
 // 或者，只是一个组件配置对象。
 // 我们晚点再讨论嵌套路由。
 const routes = [
+    { path: '/', component: Home },
     { path: '/home', component: Home },
     { path: '/about', component: About }
 ]
