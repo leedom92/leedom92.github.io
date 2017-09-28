6066969183bed081b9d2dff2a269c34882baf1e3
leedom <jld1221@163.com>
2017-09-28 21:46:42 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,15 @@ Vue.component('el-nav',{
     },
     methods:{
         returnFalse:function(){
-            document.getElementById('collapseBtn').onclick();
+            $('#collapse').addClass("collapsed");  
+            $('#collapse').attr("aria-expanded",false);  
+            $("#navbar").removeClass("in");  
+            $("#navbar").attr("aria-expanded",false);  
+
+            document.getElementById('collapseBtn').setAttribute("class","collapsed");
+            document.getElementById('collapseBtn').setAttribute("aria-expanded",false);
+            document.getElementById('bs-example-navbar-collapse-1').removeAttribute("class");
+            document.getElementById('bs-example-navbar-collapse-1').setAttribute("aria-expanded",false);
         }
     },
     template: `<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
