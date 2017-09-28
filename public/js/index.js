41c19f3e7d485b449412b9e658c1fa27c18166f4
leedom <jld1221@163.com>
2017-09-28 21:06:35 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ Vue.component('el-nav',{
     template: `<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
                 <div class="container">
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'+
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
