211d152c4926e51f40b83a11fa087620b1deefde
leedom <jld1221@163.com>
2017-09-28 17:03:58 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,33 @@
 //nav 组件
 Vue.component('el-nav',{
-    template: '<nav class="navbar navbar-default navbar-fixed-top" role="navigation">'+
-                '<div class="container">'+
-                    '<div class="navbar-header">'+
-                        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'+
-                            '<span class="sr-only">Toggle navigation</span>'+
-                            '<span class="icon-bar"></span>'+
-                            '<span class="icon-bar"></span>'+
-                            '<span class="icon-bar"></span>'+
-                        '</button>'+
-                        '<a class="navbar-brand" href="#">JLD</a>'+
-                    '</div>'+
-                    '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
-                        '<ul class="nav navbar-nav">'+
-                            '<li><router-link to="/home">home</router-link></li>'+
-                            '<li><router-link to="/about">about</router-link></li>'+
-                            '<li class="dropdown">'+
-                                '<a href="#" class="dropdown-toggle" data-toggle="dropdown">More</a>'+
-                                '<ul class="dropdown-menu" role="menu">'+
-                                    '<li><a href="#">Connection</a></li>'+
-                                    '<li><a>Experience</a></li>'+
-                                    '<li class="divider"></li>'+
-                                    '<li><router-link to="/entertainment">Entertainment</router-link></li>'+
-                                '</ul>'+
-                            '</li>'+
-                        '</ul>'+
-                    '</div>'+
-                '</div>'+
-            '</nav>'
+    template: `<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'+
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>'
+                        <a class="navbar-brand" href="#">JLD</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><router-link to="/home">home</router-link></li>
+                            <li><router-link to="/about">about</router-link></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">More</a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Connection</a></li>
+                                    <li><a>Experience</a></li>
+                                    <li class="divider"></li>
+                                    <li><router-link to="/entertainment">Entertainment</router-link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>`
 })
 
 Vue.component('el-header',{
