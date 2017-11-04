24f5e0ac6305292d5182a9065c83fca427f3de5e
leedom <jld1221@163.com>
2017-11-04 13:12:09 +0800

    test

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,18 +3,3 @@ Vue.filter('timeFunc',function(val){
     formatDate(val)
 })
 
-function formatDate(a)   {
-    var   year=a.getFullYear();
-    var   month=checkTime(a.getMonth()+1);
-    var   date=checkTime(a.getDate());
-    var   hour=checkTime(a.getHours());
-    var   minute=checkTime(a.getMinutes());
-    return   year+"-"+month+"-"+date+" "+hour+":"+minute;
-}
-
-function checkTime(i){
-    if (i<10){
-        i="0" + i
-    }
-    return i
-}
