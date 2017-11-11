dda86a059989a7b041f4ede37e616c5630b87273
leedom <jld1221@163.com>
2017-11-11 10:07:52 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -127,11 +127,6 @@ Vue.component('el-time', {
             placement="right"
             width="400"
             trigger="click">
-            <el-table :data="gridData">
-              <el-table-column width="150" property="date" label="日期"></el-table-column>
-              <el-table-column width="100" property="name" label="姓名"></el-table-column>
-              <el-table-column width="300" property="address" label="地址"></el-table-column>
-            </el-table>
           </el-popover>
           
           <el-button v-popover:popover4>click 激活</el-button>
